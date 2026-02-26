a2374f11cec235a4578c7c696b9427a8762cb392
sylajen <sailajansiv@gmail.com>
2026-02-26 02:01:52 -0500

    Changed sizing and colour

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,10 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
           src="/logos/Scluttr.svg"
           alt="Scluttr logo"
-          width={100}
-          height={20}
+          width={200}
+          height={40}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
